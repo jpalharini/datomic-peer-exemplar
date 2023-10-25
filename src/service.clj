935ec0380fbfb638b00c8eb0e5899b24daea7616
Joao Palharini <joao@palharini.me>
2023-10-25 15:02:30 -0300

    first commit

---
+++ src/service.clj
@@ -0,0 +1,25 @@
+(ns service
+  (:require [datomic.api :as d]
+            [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]))
+
+(defn is-datomic-up? [{{:keys [db-uri]} :query-params}]
+  (try
+    (d/create-database db-uri)
+    (d/connect db-uri)
+    {:status 200 :body "Yes!"}
+    (catch Exception _
+      {:status 503 :body "Not yet!"})))
+
+(def routes
+  (route/expand-routes
+   #{["/check-datomic" :get is-datomic-up?
+      :route-name :check-datomic]}))
+
+(defn create-server []
+  (http/create-server {::http/routes routes
+                       ::http/type   :jetty
+                       ::http/port   8090}))
+
+(defn start! []
+  (http/start (create-server)))
